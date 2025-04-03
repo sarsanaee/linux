7b6e926e5dcd766ad634f4e1ed3a72131c7b96f9
Alireza Sanaee <alireza.sanaee@huawei.com>
2025-04-03 17:26:20 +0100

    DT: enhance cpu-map creation for SMT threads sharing resources
    Previously, a patch (DEPEND-ON tag) enabled the sharing of resources,
    such as L1 Cache and clocks, between SMT threads. However, the cpu-map
    object requires each thread to point to a CPU node in order to be
    created properly. This patch addresses that issue.
    
    It introduces a fix that uses thread IDs to match each CPU thread in the
    register array within the cpu-node. This ensures that the cpu-map is
    correctly built even when SMT threads share resources.
    
    Also, I used a new variable in CPU node "**#cpu-cell**", in which I
    describe the number of parameters when parsing the phandle with arg to
    find the local thread ID. This vairable is not mandatary, and is indeed
    backward-compatible.
    
    Additionally, based on the discussions in [1], I adopted the new cpu-map
    layout, where the first parameter is a phandle and the second is the
    local thread index, as shown below:
    
        core0 {
          thread0 {
            cpu = <&cpu0 0>;
          };
          thread1 {
            cpu = <&cpu0 1>;
          };
        };
    
    [1] https://lore.kernel.org/devicetree-spec/CAL_JsqK1yqRLD9B+G7UUp=D8K++mXHq0Rmv=1i6DL_jXyZwXAw@mail.gmail.com/
    
    Signed-off-by: Alireza Sanaee <alireza.sanaee@huawei.com>

--- drivers/base/arch_topology.c
+++ drivers/base/arch_topology.c
@@ -519,19 +519,35 @@ core_initcall(free_raw_capacity);
 static int __init get_cpu_for_node(struct device_node *node)
 {
 	int cpu;
-	struct device_node *cpu_node __free(device_node) =
-		of_parse_phandle(node, "cpu", 0);
+	struct of_phandle_args args;
+	int ret = -1;
+	int thread_index;
 
-	if (!cpu_node)
-		return -1;
+	/* Check whether #cpu-cells exists, if yes then find the param,
+	 * otherwise, traditional approach no indices.
+	 */
+	ret = of_parse_phandle_with_args(node, "cpu",
+					 "#cpu-cells",
+					 0, &args);
 
-	cpu = of_cpu_node_to_id(cpu_node, 0);
+	if (ret < 0) {
+		ret = of_parse_phandle_with_args(node, "cpu",
+						 NULL,
+					 	 0, &args);
+		if (ret < 0)
+			return -1;
+	}
+
+	thread_index = args.args_count == 1 ? args.args[0] : 0;
+
+	cpu = of_cpu_node_to_id(args.np, thread_index);
 	if (cpu >= 0)
-		topology_parse_cpu_capacity(cpu_node, cpu);
+		topology_parse_cpu_capacity(args.np, cpu);
 	else
 		pr_info("CPU node for %pOF exist but the possible cpu range is :%*pbl\n",
-			cpu_node, cpumask_pr_args(cpu_possible_mask));
+			args.np, cpumask_pr_args(cpu_possible_mask));
 
+	of_node_put(args.np);
 	return cpu;
 }
 
